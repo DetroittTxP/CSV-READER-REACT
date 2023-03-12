b46d7a3b94113234ebeb66527c7448a9cf1e0d6a
Parame Srisomsap <s6304062616170@email.kmutnb.ac.th>
2023-03-12 22:25:08 +0700

    2

--- read-csv-react/src/components/2.js
+++ read-csv-react/src/components/2.js
@@ -1,21 +1,57 @@
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect, useState } from 'react';
+import { Chart1 } from './chart/chart1';
 export const  Csv2=()=>{
     const [data,Setdata] = useState([]);
-
+    const [total,Settotal] = useState(null)
+    
     useEffect(()=>{
         axios.get('https://covid19.ddc.moph.go.th/api/Cases/today-cases-by-provinces')
-        .then(res=>Setdata(res.data)).catch(err=>alert(err))
+        .then(res=>{
+            const data = res.data
+            Setdata(data)
+            const total = [];
+            let sum_death = 0;
+            let sum_new_case = 0,total_case =  0,new_case_excludeabroad = 0,total_case_excludeabroad = 0,total_death = 0
+            data.forEach((e)=>{
+                sum_new_case += e.new_case;
+                total_case+=e.total_case;
+                new_case_excludeabroad += e.new_case_excludeabroad;
+                total_case_excludeabroad += e.total_case_excludeabroad;
+                sum_death += e.new_death
+                total_death+=e.total_death
+            })
+            total.push(sum_new_case);
+            total.push(total_case);
+            total.push(new_case_excludeabroad)
+            total.push(total_case_excludeabroad)
+            total.push(sum_death)
+            total.push(total_death)
+             
+            Settotal(total)
+            
+        }).catch(err=>alert(err))
+
+        // data.forEach((e)=>{
+        //     if(e.new_death !== 0){
+        //         Settotald(sum_death += e.new_death)
+        //     }
+        // })
+
+        
     },[])
+
+    
 
     return(
         <div>
             <h3>รายงานสถานการณ์ COVID-19 ประจำสัปดาห์ แยกตามรายจังหวัด</h3>
+            
+            <Chart1/>
 
             <br/>
             <br/>
-
             <table className='table' style={{backgroundColor:'white',marginLeft:160,width:1500}}>
                   <thead>
                        <tr>
---
+++ read-csv-react/src/components/chart/chart1.js
@@ -0,0 +1,29 @@
+import { Bar } from 'react-chartjs-2';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+  export const Chart1 = ()=>{
+        return(
+            <div>testchart</div>
+        )
+  }
+
+
+
+
